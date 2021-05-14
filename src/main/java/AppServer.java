f1f63a5e5da061756e392ede9a59243d4df60f80
Noe Brito <n.o.brito@hotmail.com>
2021-05-13 18:49:53 -0700

    GH-1 - initial bootstrap of java project

---
+++ src/main/java/AppServer.java
@@ -0,0 +1,23 @@
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * App server class.
+ */
+public class AppServer {
+
+  private static final Logger logger = LoggerFactory.getLogger(AppServer.class);
+
+  /**
+   * Runs the app server.
+   *
+   * @param port The port that the server exposes.
+   */
+  public void run(final int port) {
+    final Javalin app = Javalin.create()
+      .start(port);
+
+    logger.info("Application started and is listening on port " + port);
+  }
+}
