32336db867ecc5798cf3c613725f1cb7f26e80a6
Noe Brito <n.o.brito@hotmail.com>
2021-05-19 21:07:55 -0700

    GH-10 - added typesafe config

--- src/main/java/org/openbrewerydb/Application.java
+++ src/main/java/org/openbrewerydb/Application.java
@@ -3,6 +3,7 @@ package org.openbrewerydb;
 import java.time.Clock;
 import org.jdbi.v3.core.Jdbi;
 import org.openbrewerydb.api.BreweryApi;
+import org.openbrewerydb.config.Configuration;
 import org.openbrewerydb.config.DbConfig;
 import org.openbrewerydb.dal.BreweryDao;
 import org.openbrewerydb.dal.BreweryDaoImpl;
@@ -19,16 +20,9 @@ public class Application {
    */
   public static void main(String[] args) {
 
-    // TODO: Add typesafe config
-
-    // get db configuration and run migrations.
-    final String dbHost = System.getenv("DB_HOST");
-    final int dbPort = Integer.parseInt(System.getenv("DB_PORT"));
-    final String dbName = System.getenv("DB_NAME");
-    final String dbSchema = System.getenv("DB_SCHEMA");
-    final String dbUserName = System.getenv("DB_USER");
-    final String dbPassword = System.getenv("DB_PASSWORD");
-    final DbConfig dbConfig = new DbConfig(dbHost, dbPort, dbSchema, dbName, dbUserName, dbPassword);
+    final Configuration config = new Configuration();
+    config.load();
+    final DbConfig dbConfig = config.getDbConfig();
     DatabaseUtils.performMigrations(dbConfig);
 
     final Jdbi jdbi = Jdbi.create(dbConfig.toUrl());
@@ -37,6 +31,6 @@ public class Application {
     final BreweryApi breweryApi = new BreweryApi(breweryService);
 
     final AppServer appServer = new AppServer(breweryApi);
-    appServer.run(10000);
+    appServer.run(config.getPort());
   }
 }
--- src/main/java/org/openbrewerydb/config/DbConfig.java
+++ src/main/java/org/openbrewerydb/config/DbConfig.java
@@ -3,14 +3,27 @@ package org.openbrewerydb.config;
 /**
  * DB config record.
  */
-public record DbConfig(
-        String host,
-        int port,
-        String databaseName,
-        String schema,
-        String userName,
-        String password
-) {
+public class DbConfig {
+  private String host;
+  private int port;
+  private String databaseName;
+  private String schema;
+  private String userName;
+  private String password;
+
+  public DbConfig() {}
+
+  /**
+   * Constructor.
+   */
+  public DbConfig(String host, int port, String databaseName, String schema, String userName, String password) {
+    this.host = host;
+    this.port = port;
+    this.databaseName = databaseName;
+    this.schema = schema;
+    this.userName = userName;
+    this.password = password;
+  }
 
   /**
    * Maps the db config to a jdbc postgres url.
@@ -19,6 +32,54 @@ public record DbConfig(
    */
   public String toUrl() {
     return String.format("jdbc:postgresql://%s:%s/%s?currentSchema=%s&user=%s&password=%s",
-      this.host, this.port, this.databaseName(), this.schema(), this.userName(), this.password());
+      this.host, this.port, this.databaseName, this.schema, this.userName, this.password);
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public String getDatabaseName() {
+    return databaseName;
+  }
+
+  public String getSchema() {
+    return schema;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public void setDatabaseName(String databaseName) {
+    this.databaseName = databaseName;
+  }
+
+  public void setSchema(String schema) {
+    this.schema = schema;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 }
